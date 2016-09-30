e823c8af1ecfec633a618d3cc0cdc1b826c57af6
Jeremy Worboys <jw@jeremyworboys.com>
2016-09-30 23:52:11 +1000

    Initial work on registration form

---
+++ Form/RegistrationFormType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RegistrationFormType extends AbstractType
+{
+    /** @var string */
+    private $userClass;
+
+    /**
+     * RegistrationFormType constructor.
+     *
+     * @param string $userClass
+     */
+    public function __construct(string $userClass)
+    {
+        $this->userClass = $userClass;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array                                        $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Email Address',
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type'           => PasswordType::class,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm password'],
+            ])
+            ->add('accept_terms', CheckboxType::class, [
+                'label'       => 'I accept the terms of service',
+                'mapped'      => false,
+                'constraints' => [
+                    new Assert\IsTrue(),
+                ],
+            ]);
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => $this->userClass,
+        ]);
+    }
+}
