31db1fe063150d8e8e00628d3831a3819e9dcc2e
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 01:32:37 +1000

    Load bundle doctrine mapping

--- WarbleMediaPhoenixBundle.php
+++ WarbleMediaPhoenixBundle.php
@@ -2,8 +2,33 @@
 
 namespace WarbleMedia\PhoenixBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class WarbleMediaPhoenixBundle extends Bundle
 {
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $this->addRegisterMappingsPass($container);
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    private function addRegisterMappingsPass(ContainerBuilder $container)
+    {
+        $mappings = [
+            realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'WarbleMedia\PhoenixBundle\Model',
+        ];
+
+        if (class_exists(DoctrineOrmMappingsPass::class)) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
+    }
 }
