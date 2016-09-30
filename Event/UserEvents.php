33b9615fd19ac28728922df30d19ec90e40a678c
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 01:19:08 +1000

    Add registration completed event

--- Event/UserEvents.php
+++ Event/UserEvents.php
@@ -12,4 +12,13 @@ final class UserEvents
      * @Event("WarbleMedia\PhoenixBundle\Event\FormEvent")
      */
     const REGISTRATION_SUCCESS = 'warble_media_phoenix.registration.success';
+
+    /**
+     * The REGISTRATION_COMPLETED event occurs after saving the user in the registration process.
+     *
+     * This event allows you to access the response which will be sent.
+     *
+     * @Event("WarbleMedia\PhoenixBundle\Event\UserResponseEvent")
+     */
+    const REGISTRATION_COMPLETED = 'warble_media_phoenix.registration.completed';
 }
