d97e1b4ef988ad5773a564b7968307ab0a98d2c4
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 01:05:27 +1000

    Dispatch event

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -4,6 +4,8 @@ namespace WarbleMedia\PhoenixBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use WarbleMedia\PhoenixBundle\Event\FormEvent;
+use WarbleMedia\PhoenixBundle\Event\UserEvents;
 
 class RegistrationController extends Controller
 {
@@ -13,6 +15,7 @@ class RegistrationController extends Controller
      */
     public function registerAction(Request $request)
     {
+        $dispatcher = $this->get('event_dispatcher');
         $userManager = $this->get('warble_media_phoenix.model.user_manager');
         $formFactory = $this->get('warble_media_phoenix.form.registration_factory');
 
@@ -23,6 +26,9 @@ class RegistrationController extends Controller
         $form->setData($user);
 
         if ($form->handleRequest($request)->isValid()) {
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(UserEvents::REGISTRATION_SUCCESS, $event);
+
             // TODO: Handle submitted form...
         }
 
