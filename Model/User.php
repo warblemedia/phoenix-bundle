44f5c845df2c7a31969bfc4fd6f3b5c3cfd28893
Jeremy Worboys <jw@jeremyworboys.com>
2016-09-30 13:44:47 +1000

    Add initial user fields

--- Model/User.php
+++ Model/User.php
@@ -4,5 +4,90 @@ namespace WarbleMedia\PhoenixBundle\Model;
 
 abstract class User implements UserInterface
 {
+    /** @var mixed */
+    protected $id;
 
+    /** @var string */
+    protected $name;
+
+    /** @var string */
+    protected $email;
+
+    /** @var string */
+    protected $password;
+
+    /** @var string */
+    protected $plainPassword;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     */
+    public function setPlainPassword(string $plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+    }
 }
--- Model/UserInterface.php
+++ Model/UserInterface.php
@@ -7,4 +7,43 @@ use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 interface UserInterface extends AdvancedUserInterface
 {
 
+    /**
+     * @return mixed
+     */
+    public function getId();
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name);
+
+    /**
+     * @return string
+     */
+    public function getEmail();
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email);
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password);
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword();
+
+    /**
+     * @param string $password
+     */
+    public function setPlainPassword(string $password);
 }
