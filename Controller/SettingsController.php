0d9772aa6e938f6cc0e53e50b7bc4160603be4d4
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 19:13:56 +1000

    Stub settings actions

---
+++ Controller/SettingsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SettingsController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function profileAction()
+    {
+        // TODO: Implement profileAction() method.
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function securityAction()
+    {
+        // TODO: Implement securityAction() method.
+    }
+}
