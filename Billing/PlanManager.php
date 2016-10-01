c31381f689dd0dba4ad16193c63d2b6f1d0bde13
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-02 01:34:47 +1000

    Add query methods to plan manager

--- Billing/PlanManager.php
+++ Billing/PlanManager.php
@@ -17,4 +17,55 @@ class PlanManager implements PlanManagerInterface
         // TODO: Allow some way of using a custom plan implementation
         $this->plans[] = new Plan($id, $name, $options);
     }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getMonthlyPlans()
+    {
+        return $this->filterPlans(function (PlanInterface $plan) {
+            return $plan->getInterval() === 'monthly';
+        });
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getPaidMonthlyPlans()
+    {
+        return $this->filterPlans(function (PlanInterface $plan) {
+            return $plan->getPrice() > 0 &&
+                   $plan->getInterval() === 'monthly';
+        });
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getYearlyPlans()
+    {
+        return $this->filterPlans(function (PlanInterface $plan) {
+            return $plan->getInterval() === 'yearly';
+        });
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getPaidYearlyPlans()
+    {
+        return $this->filterPlans(function (PlanInterface $plan) {
+            return $plan->getPrice() > 0 &&
+                   $plan->getInterval() === 'yearly';
+        });
+    }
+
+    /**
+     * @param string $interval
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    private function filterPlans(callable $filter)
+    {
+        return array_filter($this->plans, $filter);
+    }
 }
--- Billing/PlanManagerInterface.php
+++ Billing/PlanManagerInterface.php
@@ -4,5 +4,23 @@ namespace WarbleMedia\PhoenixBundle\Billing;
 
 interface PlanManagerInterface
 {
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getMonthlyPlans();
 
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getPaidMonthlyPlans();
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getYearlyPlans();
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Billing\PlanInterface[]
+     */
+    public function getPaidYearlyPlans();
 }
