a06d89ff858d84760c3b7808d58447188372165f
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 17:06:36 +1000

    Reset users password

--- Controller/ResettingController.php
+++ Controller/ResettingController.php
@@ -4,6 +4,7 @@ namespace WarbleMedia\PhoenixBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use WarbleMedia\PhoenixBundle\Mailer\Mail\ResettingMail;
 
 class ResettingController extends Controller
 {
@@ -21,5 +22,25 @@ class ResettingController extends Controller
      */
     public function sendEmailAction(Request $request)
     {
+        $userManager = $this->get('warble_media_phoenix.model.user_manager');
+        $tokenGenerator = $this->get('warble_media_phoenix.util.token_generator');
+        $tokenTimeout = $this->getParameter('warble_media_phoenix.resetting.token_ttl');
+
+        $user = $userManager->findUserByEmail($request->request->get('email'));
+
+        if ($user !== null && !$user->isPasswordRequestNonExpired($tokenTimeout)) {
+            if ($user->getConfirmationToken() === null) {
+                $user->setConfirmationToken($tokenGenerator->generateToken());
+            }
+
+            $user->setPasswordRequestedAt(new \DateTime());
+            $this->get('warble_media_phoenix.mailer')->send(new ResettingMail($user));
+
+            $userManager->updateUser($user);
+        }
+
+        return $this->redirectToRoute('warble_media_phoenix_resetting_check_email', [
+            'email' => $user->getEmail(),
+        ]);
     }
 }
