ef7faedeac4dba60e9298a375080a302fe18d40b
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-01 20:00:06 +1000

    Initial work on edit profile action

--- Controller/SettingsController.php
+++ Controller/SettingsController.php
@@ -3,15 +3,57 @@
 namespace WarbleMedia\PhoenixBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use WarbleMedia\PhoenixBundle\Event\FormEvent;
+use WarbleMedia\PhoenixBundle\Event\UserEvents;
+use WarbleMedia\PhoenixBundle\Event\UserRequestEvent;
+use WarbleMedia\PhoenixBundle\Event\UserResponseEvent;
+use WarbleMedia\PhoenixBundle\Model\UserInterface;
 
 class SettingsController extends Controller
 {
     /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function profileAction()
+    public function profileAction(Request $request)
     {
-        // TODO: Implement profileAction() method.
+        $user = $this->getUserOrError();
+        $dispatcher = $this->get('event_dispatcher');
+        $userManager = $this->get('warble_media_phoenix.model.user_manager');
+        $formFactory = $this->get('warble_media_phoenix.form.profile_factory');
+
+        $event = new UserRequestEvent($user, $request);
+        $dispatcher->dispatch(UserEvents::PROFILE_EDIT_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ($form->handleRequest($request)->isValid()) {
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(UserEvents::PROFILE_EDIT_SUCCESS, $event);
+
+            $userManager->updateUser($user);
+
+            $response = $event->getResponse();
+            if ($response === null) {
+                $response = $this->redirectToRoute('warble_media_phoenix_settings_profile');
+            }
+
+            $event = new UserResponseEvent($user, $request, $response);
+            $dispatcher->dispatch(UserEvents::PROFILE_EDIT_COMPLETED, $event);
+
+            return $response;
+        }
+
+        return $this->render('WarbleMediaPhoenixBundle:Settings:profile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
@@ -20,5 +62,19 @@ class SettingsController extends Controller
     public function securityAction()
     {
         // TODO: Implement securityAction() method.
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\UserInterface
+     */
+    private function getUserOrError()
+    {
+        $user = $this->getUser();
+
+        if ($user instanceof UserInterface) {
+            return $user;
+        }
+
+        throw $this->createAccessDeniedException('This user does not have access to this section.');
     }
 }
--- Event/UserEvents.php
+++ Event/UserEvents.php
@@ -50,6 +50,33 @@ final class UserEvents
     const RESETTING_RESET_COMPLETED = 'warble_media_phoenix.resetting.reset.completed';
 
     /**
+     * The PROFILE_EDIT_INITIALIZE event occurs when the profile editing process is initialized.
+     *
+     * This event allows you to modify the default values of the user before binding the form.
+     *
+     * @Event("\WarbleMedia\PhoenixBundle\Event\UserRequestEvent")
+     */
+    const PROFILE_EDIT_INITIALIZE = 'fos_user.profile.edit.initialize';
+
+    /**
+     * The PROFILE_EDIT_SUCCESS event occurs when the profile edit form is submitted successfully.
+     *
+     * This event allows you to set the response instead of using the default one.
+     *
+     * @Event("\WarbleMedia\PhoenixBundle\Event\FormEvent")
+     */
+    const PROFILE_EDIT_SUCCESS = 'fos_user.profile.edit.success';
+
+    /**
+     * The PROFILE_EDIT_COMPLETED event occurs after saving the user in the profile edit process.
+     *
+     * This event allows you to access the response which will be sent.
+     *
+     * @Event("\WarbleMedia\PhoenixBundle\Event\UserResponseEvent")
+     */
+    const PROFILE_EDIT_COMPLETED = 'fos_user.profile.edit.completed';
+
+    /**
      * The SECURITY_IMPLICIT_LOGIN event occurs when the user is logged in programmatically.
      *
      * This event allows you to access the user that was authenticated.
