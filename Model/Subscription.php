78ab3a69ecbd21587e779c3ac458d6ea091c5041
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-02 13:39:01 +1100

    Add subscription model

---
+++ Model/Subscription.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Model;
+
+class Subscription implements SubscriptionInterface
+{
+
+}
---
+++ Model/SubscriptionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Model;
+
+interface SubscriptionInterface
+{
+
+}
