299d7494f9678c90fb96ddc71664b6ffc50b3e2a
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-02 22:40:32 +1100

    Add subscription methods to customer

--- Model/Customer.php
+++ Model/Customer.php
@@ -4,7 +4,7 @@ namespace WarbleMedia\PhoenixBundle\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
-abstract class Customer extends User
+abstract class Customer extends User implements CustomerInterface
 {
     /** @var \Doctrine\Common\Collections\Collection */
     protected $subscriptions;
@@ -16,5 +16,70 @@ abstract class Customer extends User
     {
         parent::__construct();
         $this->subscriptions = new ArrayCollection();
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface[]
+     */
+    public function getSubscriptions(): array
+    {
+        return $this->subscriptions->toArray();
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface|null
+     */
+    public function getSubscription(string $name = 'default')
+    {
+        foreach ($this->getSubscriptions() as $subscription) {
+            if ($subscription->getName() === $name) {
+                return $subscription;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getSubscriptionNames(): array
+    {
+        $names = [];
+
+        foreach ($this->getSubscriptions() as $subscription) {
+            $names[] = $subscription->getName();
+        }
+
+        return $names;
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasSubscription(string $name): bool
+    {
+        return in_array($name, $this->getSubscriptionNames(), true);
+    }
+
+    /**
+     * @param \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface $subscription
+     */
+    public function addSubscription(SubscriptionInterface $subscription)
+    {
+        if (!$this->subscriptions->contains($subscription)) {
+            $this->subscriptions->add($subscription);
+        }
+    }
+
+    /**
+     * @param \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface $subscription
+     */
+    public function removeSubscription(SubscriptionInterface $subscription)
+    {
+        if ($this->subscriptions->contains($subscription)) {
+            $this->subscriptions->removeElement($subscription);
+        }
     }
 }
--- Model/CustomerInterface.php
+++ Model/CustomerInterface.php
@@ -4,5 +4,34 @@ namespace WarbleMedia\PhoenixBundle\Model;
 
 interface CustomerInterface extends UserInterface
 {
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface[]
+     */
+    public function getSubscriptions(): array;
 
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface|null
+     */
+    public function getSubscription(string $name = 'default');
+
+    /**
+     * @return string[]
+     */
+    public function getSubscriptionNames(): array;
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasSubscription(string $name): bool;
+
+    /**
+     * @param \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface $subscription
+     */
+    public function addSubscription(SubscriptionInterface $subscription);
+
+    /**
+     * @param \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface $subscription
+     */
+    public function removeSubscription(SubscriptionInterface $subscription);
 }
