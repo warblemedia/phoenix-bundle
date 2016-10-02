95582c524955d96cdfa20122d667e266633e088f
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-02 22:46:36 +1100

    Initial create subscription implementation

--- Model/SubscriptionManager.php
+++ Model/SubscriptionManager.php
@@ -27,6 +27,28 @@ class SubscriptionManager implements SubscriptionManagerInterface
      */
     public function createSubscription(CustomerInterface $customer, PlanInterface $plan, bool $fromRegistration = false): SubscriptionInterface
     {
-        // TODO: Implement subscribe() method.
+        $subscription = $this->createSubscriptionInstance();
+        $subscription->setStripePlan($plan->getId());
+
+        // Set trial end date on the subscription. By default, we skip the trial for
+        // non-registration requests as the customer has usually already trialed by this point.
+        $trialDays = $plan->getTrialDays();
+        if ($fromRegistration && $trialDays > 0) {
+            $trialEndsAt = new \DateTime();
+            $trialEndsAt->modify((int) $trialDays . ' day');
+            $subscription->setTrialEndsAt($trialEndsAt);
+        }
+
+        $customer->addSubscription($subscription);
+
+        return $subscription;
+    }
+
+    /**
+     * @return \WarbleMedia\PhoenixBundle\Model\SubscriptionInterface
+     */
+    protected function createSubscriptionInstance(): SubscriptionInterface
+    {
+        return new $this->subscriptionClass;
     }
 }
