cc1ad2498fe1c9ebc9fb73b285e0e9d1e35b4236
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-02 17:54:03 +1100

    Work on stripe form script

--- Resources/public/js/stripe_form.js
+++ Resources/public/js/stripe_form.js
@@ -1,3 +1,30 @@
-(function ($, Phoenix) {
+(function ($, Stripe, Phoenix) {
+    Stripe.setPublishableKey(Phoenix.stripeKey);
 
-})(jQuery, window.Phoenix || {});
+    var form = $('#stripe_form');
+    var tokenField = $('#' + Phoenix.tokenFieldId);
+    var submitButton = form.find('[type="submit"]');
+    var submitButtonText = submitButton.text();
+    var errorContainer = form.find('.stripe-errors');
+
+    form.on('submit', function (e) {
+        e.preventDefault();
+
+        submitButton.prop('disabled', true).text('Processing...');
+        errorContainer.empty();
+
+        Stripe.card.createToken(form, function (status, response) {
+            console.log(status, response);
+            if (response.error) {
+                errorContainer.html($('<div class="alert alert-danger">' + response.error.message + '</div>'));
+                submitButton.prop('disabled', false).text(submitButtonText);
+            } else {
+                tokenField.val(response.id);
+                form.get(0).submit();
+            }
+        });
+
+        return false;
+    });
+
+})(jQuery, Stripe, window.Phoenix || {});
