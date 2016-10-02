b31d6fef06dd59f6a8bca1a0dc6571cd20cb94ef
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-03 01:18:52 +1100

    Initial work handling subscription form submission

--- Controller/BillingController.php
+++ Controller/BillingController.php
@@ -3,6 +3,9 @@
 namespace WarbleMedia\PhoenixBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use WarbleMedia\PhoenixBundle\Event\FormEvent;
+use WarbleMedia\PhoenixBundle\Event\PhoenixEvents;
+use WarbleMedia\PhoenixBundle\Event\SubscriptionResponseEvent;
 
 class BillingController extends Controller
 {
@@ -12,15 +15,34 @@ class BillingController extends Controller
      */
     public function subscriptionAction(Request $request)
     {
+        $user = $this->getUserOrError();
+        $dispatcher = $this->get('event_dispatcher');
         $formFactory = $this->get('warble_media_phoenix.form.subscription_factory');
+        $subscriptionManager = $this->get('warble_media_phoenix.model.subscription_manager');
 
         $form = $formFactory->createForm();
 
         if ($form->handleRequest($request)->isValid()) {
-            // TODO: Handle form submission...
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(PhoenixEvents::NEW_SUBSCRIPTION_SUCCESS, $event);
+
+            $data = $form->getData();
+            $customer = $user->getCustomer();
+            $subscription = $subscriptionManager->createSubscription($customer, $data['plan']);
+
+            $response = $event->getResponse();
+            if ($response === null) {
+                $response = $this->redirectToRoute('warble_media_phoenix_settings_subscription');
+            }
+
+            $event = new SubscriptionResponseEvent($subscription, $request, $response);
+            $dispatcher->dispatch(PhoenixEvents::NEW_SUBSCRIPTION_COMPLETED, $event);
+
+            return $response;
         }
 
         return $this->render('WarbleMediaPhoenixBundle:Settings:subscription.html.twig', [
+            'user' => $user,
             'form' => $form->createView(),
         ]);
     }
