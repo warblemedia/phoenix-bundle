b70eb977ea736cbd226514b5b8f25a315685dd6a
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-03 15:16:05 +1100

    Add payment method form type

---
+++ Form/PaymentMethodType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PaymentMethodType extends AbstractType
+{
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array                                        $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('stripeToken', HiddenType::class);
+    }
+
+    /**
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+        ]);
+    }
+}
