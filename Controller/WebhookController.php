e9cbf8fbcddc805786b7cb12ed9f9a7b6475f95c
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-03 17:23:10 +1100

    Add webhooks controller

---
+++ Controller/WebhookController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WarbleMedia\PhoenixBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class WebhookController extends Controller
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function stripeAction(Request $request)
+    {
+        // TODO: Implement stripeAction() method.
+    }
+}
