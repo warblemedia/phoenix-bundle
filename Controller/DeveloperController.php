40903ba665dc285b634987ed7f7a972d46939e25
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-04 22:43:22 +1100

    Show percent change since last month/year

--- Controller/DeveloperController.php
+++ Controller/DeveloperController.php
@@ -11,11 +11,19 @@ class DeveloperController extends Controller
     {
         $indicators = $this->get('warble_media_phoenix.performance.indicators');
 
+        $lastMonth = new \DateTime();
+        $lastMonth->setTime(0, 0, 0)->modify('-1 month');
+
+        $lastYear = new \DateTime();
+        $lastYear->setTime(0, 0, 0)->modify('-1 year');
+
         return $this->render('WarbleMediaPhoenixBundle:Developer:metrics.html.twig', [
             'yearlyRecurringRevenue'  => $indicators->getYearlyRecurringRevenue(),
             'monthlyRecurringRevenue' => $indicators->getMonthlyRecurringRevenue(),
             'totalRevenueToDate'      => $indicators->getTotalRevenueToDate(),
             'trialCustomerCount'      => $indicators->getTrialCustomerCount(),
+            'lastMonthsIndicators'    => $indicators->getHistoricalIndicatorsFor($lastMonth),
+            'lastYearsIndicators'     => $indicators->getHistoricalIndicatorsFor($lastYear),
         ]);
     }
 }
