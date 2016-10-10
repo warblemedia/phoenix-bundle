6821660b8663b9fe6b41853df2f002baa852219e
Jeremy Worboys <jw@jeremyworboys.com>
2016-10-10 21:29:42 +1100

    Remove unsused imports

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,6 @@
 
 namespace WarbleMedia\PhoenixBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use WarbleMedia\PhoenixBundle\Form\ChangePasswordFormType;
--- Model/MetricsManager.php
+++ Model/MetricsManager.php
@@ -3,7 +3,6 @@
 namespace WarbleMedia\PhoenixBundle\Model;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use WarbleMedia\PhoenixBundle\Performance\PerformanceIndicators;
 use WarbleMedia\PhoenixBundle\Performance\PerformanceIndicatorsInterface;
 
 class MetricsManager implements MetricsManagerInterface
